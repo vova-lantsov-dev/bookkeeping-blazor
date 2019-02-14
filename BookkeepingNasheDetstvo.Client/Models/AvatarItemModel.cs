d5680117e4ca6c3622830a46993e4f34c2fde245
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-14 22:23:54 +0200

    Feature: AvatarItemModel

---
+++ BookkeepingNasheDetstvo.Client/Models/AvatarItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookkeepingNasheDetstvo.Client.Models
+{
+    public class AvatarItemModel
+    {
+        public string Title { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        public string ViewUrl { get; set; }
+    }
+}
