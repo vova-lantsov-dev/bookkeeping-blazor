a1a46b34eadea736056530bbc44b59b23f5fce6d
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-14 22:22:51 +0200

    Feature: browser extensions

---
+++ BookkeepingNasheDetstvo.Client/Extensions/BrowserExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BookkeepingNasheDetstvo.Client.Extensions
+{
+    public static class BrowserExtensions
+    {
+        public static Task<string> ReadLocalStorageAsync(string key)
+        {
+            return JSRuntime.Current.InvokeAsync<string>("Extensions.ReadStorage", key);
+        }
+        public static Task WriteLocalStorageAsync(string key, string value)
+        {
+            return JSRuntime.Current.InvokeAsync<object>("Extensions.WriteStorage", key, value);
+        }
+        public static Task RemoveLocalStorageAsync(string key)
+        {
+            return JSRuntime.Current.InvokeAsync<object>("Extensions.RemoveStorage", key);
+        }
+        public static async Task<(string data, string extension)> GetFileData(this ElementRef elementRef)
+        {
+            var data = await JSRuntime.Current.InvokeAsync<string>("Extensions.ReadUploadedFileAsText", elementRef);
+            var fileExtension = await JSRuntime.Current.InvokeAsync<string>("Extensions.ReadUploadedFileExtension", elementRef);
+            return (data, fileExtension);
+        }
+    }
+}
