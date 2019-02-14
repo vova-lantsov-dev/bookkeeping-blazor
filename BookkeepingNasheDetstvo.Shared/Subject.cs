151f17b96b50e11abe664be41853d7adb384c08e
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-14 16:04:58 +0200

    Feature: subject model

---
+++ BookkeepingNasheDetstvo.Shared/Subject.cs
@@ -0,0 +1,17 @@
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+using MongoDB.Bson;
+
+namespace BookkeepingNasheDetstvo.Shared
+{
+    public class Subject
+    {
+        [BsonRepresentation(BsonType.ObjectId)] public string Id { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public List<IdNamePair> Children { get; set; }
+        public IdNamePair Owner { get; set; }
+        public string PlaceIdentifier { get; set; }
+        [BsonIgnoreIfDefault] public bool IsConsultation { get; set; }
+    }
+}
