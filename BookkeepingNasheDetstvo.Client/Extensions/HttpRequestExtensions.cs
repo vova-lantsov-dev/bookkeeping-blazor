65f6939a192ea80ffd368e95ff24f63a299a7ba2
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-14 22:23:22 +0200

    Feature: http extensions

---
+++ BookkeepingNasheDetstvo.Client/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.JSInterop;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BookkeepingNasheDetstvo.Client.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static async Task<(HttpStatusCode StatusCode, T Content)> GetAsync<T>(this HttpClient http, string uri, string authToken = null)
+        {
+            if (uri == null)
+                return default;
+
+            var message = new HttpRequestMessage(HttpMethod.Get, uri);
+            if (authToken != null)
+            {
+                message.Headers.TryAddWithoutValidation("Auth-Token", authToken);
+            }
+
+            try
+            {
+                var response = await http.SendAsync(message);
+                return (response.StatusCode, response.StatusCode == HttpStatusCode.OK ? Json.Deserialize<T>(await response.Content.ReadAsStringAsync()) : default);
+            }
+            catch
+            {
+                return (HttpStatusCode.InternalServerError, default);
+            }
+        }
+
+        public static async Task<(HttpStatusCode StatusCode, T Content)> PostAsync<T>(this HttpClient http, string uri, object objectToSend, string authToken = null)
+        {
+            if (uri == null || objectToSend == null)
+                return default;
+
+            var message = new HttpRequestMessage(HttpMethod.Post, uri);
+            var content = new StringContent(Json.Serialize(objectToSend), System.Text.Encoding.UTF8, "application/json");
+            if (authToken != null)
+            {
+                content.Headers.TryAddWithoutValidation("Auth-Token", authToken);
+            }
+            message.Content = content;
+
+            try
+            {
+                var response = await http.SendAsync(message);
+                return (response.StatusCode, response.StatusCode == HttpStatusCode.OK ? Json.Deserialize<T>(await response.Content.ReadAsStringAsync()) : default);
+            }
+            catch
+            {
+                return (HttpStatusCode.InternalServerError, default);
+            }
+        }
+    }
+}
