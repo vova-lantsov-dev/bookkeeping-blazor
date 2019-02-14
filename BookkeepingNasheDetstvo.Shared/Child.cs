90b68c3d9e32cd310f6eec4236d84fd766cfca6f
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-14 16:00:19 +0200

    Feature: child model

---
+++ BookkeepingNasheDetstvo.Shared/Child.cs
@@ -0,0 +1,20 @@
+using BookkeepingNasheDetstvo.Shared.Serializers;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookkeepingNasheDetstvo.Shared
+{
+    public class Child
+    {
+        [BsonRepresentation(BsonType.ObjectId)] public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public string ImageUrl { get; set; }
+        public string FatherName { get; set; }
+        public string FatherPhone { get; set; }
+        public string MotherName { get; set; }
+        public string MotherPhone { get; set; }
+        [BsonSerializer(typeof(CustomMongoDecimalSerializer))] public decimal PerHour { get; set; }
+    }
+}
