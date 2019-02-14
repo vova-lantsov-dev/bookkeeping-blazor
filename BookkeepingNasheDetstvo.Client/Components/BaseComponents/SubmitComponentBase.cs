de1a3db24fa304eb2da0cd4bd9e5941cd929547e
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-14 21:44:35 +0200

    Feature: submit component

---
+++ BookkeepingNasheDetstvo.Client/Components/BaseComponents/SubmitComponentBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BookkeepingNasheDetstvo.Client.Components
+{
+    public class SubmitComponentBase : BaseComponent
+    {
+        [Parameter]
+        protected string Base { get; set; }
+
+        protected override Task OnInitAsync()
+        {
+            return CheckAccessToken();
+        }
+
+        protected Task<string> SubmitAsync(object value)
+        {
+            return Post<string>($"/api/{Base}", value);
+        }
+
+        protected async Task DeleteAsync(string id, string navigateTo)
+        {
+            if (await Post($"/api/{Base}/delete", id))
+                UriHelper.NavigateTo(navigateTo);
+        }
+    }
+}
