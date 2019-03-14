c1217d103d70d54d90e1b4eb87ffaf8b2fb9ca63
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-03-14 21:51:20 +0200

    FIX browser extensions

--- BookkeepingNasheDetstvo.Client/Extensions/BrowserExtensions.cs
+++ BookkeepingNasheDetstvo.Client/Extensions/BrowserExtensions.cs
@@ -1,28 +1,24 @@
-﻿using Microsoft.AspNetCore.Blazor;
-using Microsoft.JSInterop;
+﻿using Microsoft.JSInterop;
 using System.Threading.Tasks;
+using Mono.WebAssembly.Interop;
 
 namespace BookkeepingNasheDetstvo.Client.Extensions
 {
     public static class BrowserExtensions
     {
+        private static readonly IJSRuntime JsRuntime = new MonoWebAssemblyJSRuntime();
+        
         public static Task<string> ReadLocalStorageAsync(string key)
         {
-            return JSRuntime.Current.InvokeAsync<string>("Extensions.ReadStorage", key);
+            return JsRuntime.InvokeAsync<string>("Extensions.ReadStorage", key);
         }
         public static Task WriteLocalStorageAsync(string key, string value)
         {
-            return JSRuntime.Current.InvokeAsync<object>("Extensions.WriteStorage", key, value);
+            return JsRuntime.InvokeAsync<object>("Extensions.WriteStorage", key, value);
         }
         public static Task RemoveLocalStorageAsync(string key)
         {
-            return JSRuntime.Current.InvokeAsync<object>("Extensions.RemoveStorage", key);
-        }
-        public static async Task<(string data, string extension)> GetFileData(this ElementRef elementRef)
-        {
-            var data = await JSRuntime.Current.InvokeAsync<string>("Extensions.ReadUploadedFileAsText", elementRef);
-            var fileExtension = await JSRuntime.Current.InvokeAsync<string>("Extensions.ReadUploadedFileExtension", elementRef);
-            return (data, fileExtension);
+            return JsRuntime.InvokeAsync<object>("Extensions.RemoveStorage", key);
         }
     }
 }
