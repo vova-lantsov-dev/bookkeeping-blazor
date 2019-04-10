47ace5bbdddc02d8464bc2a3bcf04b1a975a3b94
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-04-10 18:51:44 +0300

    NavBar trigger

--- BookkeepingNasheDetstvo.Client/wwwroot/js/main.js
+++ BookkeepingNasheDetstvo.Client/wwwroot/js/main.js
@@ -1,17 +1,4 @@
 ﻿window.Extensions = {
-    ReadUploadedFileAsText: function (inputFile) {
-        const temporaryFileReader = new FileReader();
-        return new Promise((resolve, reject) => {
-            temporaryFileReader.onerror = () => {
-                temporaryFileReader.abort();
-                reject(new DOMException("Problem parsing input file."));
-            };
-            temporaryFileReader.addEventListener("load", function () {
-                resolve(temporaryFileReader.result.split(",")[1]);
-            }, false);
-            temporaryFileReader.readAsDataURL(inputFile.files[0]);
-        });
-    },
     ReadStorage: function (key) {
         return localStorage.getItem(key);
     },
@@ -20,5 +7,14 @@
     },
     RemoveStorage: function (key) {
         localStorage.removeItem(key);
+    }
+};
+
+function triggerNavBar() {
+    const x = document.getElementById("nav-bar");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
     }
 }
