7f8893cd0b79e8c9393f1b9c3e5fec51bb875c05
Vova Lantsov <vova.lantsov.dev@gmail.com>
2019-04-15 13:25:20 +0300

    FEATURE id name pair + id fullname pair

---
+++ BookkeepingNasheDetstvo.Client/Models/IdFullNamePair.cs
@@ -0,0 +1,11 @@
+namespace BookkeepingNasheDetstvo.Client.Models
+{
+    public sealed class IdFullNamePair
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ BookkeepingNasheDetstvo.Client/Models/IdNamePair.cs
@@ -0,0 +1,9 @@
+namespace BookkeepingNasheDetstvo.Client.Models
+{
+    public sealed class IdNamePair
+    {
+        public string Id { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
