92f0d9c0537ab9232aed177e1494d9981b73d982
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-05-28 12:49:46 +0300

    INIT logger extensions on client

---
+++ BookkeepingNasheDetstvo.Client/Extensions/LoggerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+using System;
+
+namespace BookkeepingNasheDetstvo.Client.Extensions
+{
+    internal static class LoggerExtensions
+    {
+        internal static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        internal static void Log(object obj)
+        {
+            Console.WriteLine(Json.Serialize(obj));
+        }
+    }
+}
